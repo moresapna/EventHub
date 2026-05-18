f3c954422004657f7a356d1e0ff1b29902b5602d
moresapna <sapnamore1105@gmail.com>
2026-05-18 19:44:19 +0530

    fix vercel server

--- Backend/server.js
+++ Backend/server.js
@@ -44,8 +44,10 @@ app.use("/api/payment", paymentRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+export default app;

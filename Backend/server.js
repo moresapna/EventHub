b8a99d7d581336d6ae5ee6d3899bfe348a7bb328
moresapna <sapnamore1105@gmail.com>
2026-09-09 01:37:04 +0530

    Configure EventHub backend for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,7 @@
+import dns from "node:dns";
+
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -49,4 +53,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
